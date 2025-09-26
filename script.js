61f6eb99be60b569be04ae4dc66957f7288ca38c
kimjiwoow <jiwoo7259@gmail.com>
2025-09-26 14:52:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // !!! 중요: README.md 파일을 읽고, 배포된 자신의 Google Apps Script 웹 앱 URL로 변경하세요.
-    const WEB_APP_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyS5w2IzYpAUIx3Guy6iKN44tp1cblbuSWZPp2K0FNCuMDJ2HBcOK9SXYiWR_MmI_lV/exec';
 
     const recordForm = document.getElementById('record-form');
     const recordsContainer = document.getElementById('records-container');
@@ -169,3 +169,4 @@ document.addEventListener('DOMContentLoaded', () => {
     // 초기 데이터 로드
     loadRecords();
 });
+
